730abef9e57cbfc83522e1e113030ec05f4575c4
Jenn Gonzalez <jgonz9873@gmail.com>
2019-11-18 22:54:28 -0800

    added additional projects

---
+++ readme.txt
@@ -0,0 +1,5 @@
+To do:
+
+1. convert to react project
+2. check a11y checklist and colorblindness
+3. add css styles and transitions
--- script.js
+++
@@ -1,3 +0,0 @@
-// plan: 
-
-// adding jquery to project description, title will show, then user can click on a button/link to expand details
